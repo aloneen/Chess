9325979e995a114bed340a6490578a540c424fd1
Darhan_Semiatov <Darhansemyatov1@gmail.com>
2025-04-30 00:11:00 +0500

    Update GameOverScreen.java for background also

--- core/src/main/java/com/mygdx/chess/screens/GameOverScreen.java
+++ core/src/main/java/com/mygdx/chess/screens/GameOverScreen.java
@@ -1,3 +1,4 @@
+
 package com.mygdx.chess.screens;
 
 import com.badlogic.gdx.Gdx;
@@ -5,6 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -16,73 +18,69 @@ import com.mygdx.chess.ChessGame;
 
 public class GameOverScreen implements Screen {
     private final ChessGame game;
-    private final String message;
     private final Stage stage;
     private final Skin skin;
 
     public GameOverScreen(ChessGame game, String message) {
-        this.game = game;
-        this.message = message;
+        this.game  = game;
         this.stage = new Stage(new ScreenViewport());
-        // Use your same ui skin as above or another skin if you want a different style
-        this.skin = new Skin(Gdx.files.internal("skins/uiskin.json"));
+        this.skin  = new Skin(Gdx.files.internal("skins/uiskin.json"));
 
-        // Create a table to position UI elements
+        // background
+        Image bg = new Image(new com.badlogic.gdx.graphics.Texture(Gdx.files.internal("images/main_bg.jpg")));
+        bg.setFillParent(true);
+        stage.addActor(bg);
+
+        // UI
         Table table = new Table();
         table.setFillParent(true);
         stage.addActor(table);
 
-        // Create the label for the checkmate/stalemate message
-        Label messageLabel = new Label(message, skin, "default");
-        messageLabel.setFontScale(1.5f);
-        messageLabel.setAlignment(Align.center);
+        Label msg = new Label(message, skin);
+        msg.setFontScale(1.5f);
+        msg.setAlignment(Align.center);
 
-        // Create a "NEW GAME" button
-        TextButton newGameButton = new TextButton("NEW GAME", skin);
-
-        // On click, return to the main menu or start a fresh game
-        newGameButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                // Option 1: Return to the Main Menu
-                // game.setScreen(new MainMenuScreen(game));
-
-                // Option 2: Start a fresh new GameScreen
-                game.setScreen(new GameScreen(game));
+        TextButton playWhite = new TextButton("Play as White", skin);
+        playWhite.addListener(new ClickListener() {
+            @Override public void clicked(InputEvent e, float x, float y) {
+                game.setScreen(new GameScreen(game, false));
             }
         });
 
-        // Layout in the table
-        table.add(messageLabel).center().padBottom(50f);
-        table.row();
-        table.add(newGameButton).center();
+        TextButton playBlack = new TextButton("Play as Black", skin);
+        playBlack.addListener(new ClickListener() {
+            @Override public void clicked(InputEvent e, float x, float y) {
+                game.setScreen(new GameScreen(game, true));
+            }
+        });
+
+        TextButton quit = new TextButton("Quit", skin);
+        quit.addListener(new ClickListener() {
+            @Override public void clicked(InputEvent e, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        table.add(msg).padBottom(30f).row();
+        table.add(playWhite).width(180).pad(5).row();
+        table.add(playBlack).width(180).pad(5).row();
+        table.add(quit).width(180).pad(5);
     }
 
-    @Override
-    public void show() {
+    @Override public void show() {
         Gdx.input.setInputProcessor(stage);
     }
 
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+    @Override public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         stage.act(delta);
         stage.draw();
     }
 
-    @Override public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
-    }
-    @Override public void pause() { }
-    @Override public void resume() { }
-    @Override public void hide() { }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-        skin.dispose();
-    }
+    @Override public void resize(int w,int h)    { stage.getViewport().update(w,h,true); }
+    @Override public void pause()                { }
+    @Override public void resume()               { }
+    @Override public void hide()                 { }
+    @Override public void dispose()              { stage.dispose(); skin.dispose(); }
 }
-
