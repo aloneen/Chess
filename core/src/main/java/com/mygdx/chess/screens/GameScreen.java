7266ca24e671824278a33b43ba9c5328803ed970
Darhan_Semiatov <Darhansemyatov1@gmail.com>
2025-04-30 12:30:55 +0500

    Update GameScreen.java include Factory method

--- core/src/main/java/com/mygdx/chess/screens/GameScreen.java
+++ core/src/main/java/com/mygdx/chess/screens/GameScreen.java
@@ -1,4 +1,3 @@
-// GameScreen.java
 package com.mygdx.chess.screens;
 
 import com.badlogic.gdx.Gdx;
@@ -6,52 +5,66 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.chess.actors.ChessBoard;
-import com.mygdx.chess.input.ChessInputProcessor;
 import com.mygdx.chess.ChessGame;
+import com.mygdx.chess.factory.BoardModelFactory;
+import com.mygdx.chess.input.ChessInputProcessor;
+import com.mygdx.chess.input.IGameInputProcessor;
+import com.mygdx.chess.model.IBoardModel;
+import com.mygdx.chess.view.IChessRenderer;
 
+import static com.mygdx.chess.util.BoardConfig.BOARD_SIZE;
+
+/**
+ * Renders a chess game using a separate model and renderer.
+ */
 public class GameScreen implements Screen {
-    private final SpriteBatch batch;
-    private final OrthographicCamera camera;
-    private final ChessBoard chessBoard;
+    private final ChessGame            game;
+    private final SpriteBatch          batch;
+    private final OrthographicCamera   camera;
+    private final IBoardModel          model;
+    private final IChessRenderer       renderer;
+    private final IGameInputProcessor  inputProcessor;
 
-    /** Standard constructor: white on bottom. */
-    public GameScreen(ChessGame game) {
-        this(game, false);
+    /**
+     * @param flipY false = white on bottom; true = black on bottom
+     */
+    public GameScreen(ChessGame game, boolean flipY) {
+        this.game   = game;
+        this.batch  = new SpriteBatch();
+        this.camera = new OrthographicCamera();
+        camera.setToOrtho(false, BOARD_SIZE, BOARD_SIZE);
+
+        //this.model          = new com.mygdx.chess.model.BoardModel(flipY); <- whitout Factory method.
+
+        this.model = BoardModelFactory.createStandardBoard(flipY);
+        this.renderer       = new com.mygdx.chess.view.ChessRenderer(model);
+        this.inputProcessor = new ChessInputProcessor(game, model, camera, renderer);
+        Gdx.input.setInputProcessor(inputProcessor);
     }
 
     /**
-     * @param flipY
-     *   false = white on bottom (default)
-     *   true  = black on bottom
+     * Constructor for resuming a game with existing model & renderer (e.g. after promotion).
      */
-    public GameScreen(ChessGame game, boolean flipY) {
-        batch      = new SpriteBatch();
-        camera     = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 800);
-        chessBoard = new ChessBoard(flipY);
-        Gdx.input.setInputProcessor(new ChessInputProcessor(game, chessBoard, camera));
-    }
+    public GameScreen(ChessGame game, IBoardModel model, IChessRenderer renderer) {
+        this.game   = game;
+        this.batch  = new SpriteBatch();
+        this.camera = new OrthographicCamera();
+        camera.setToOrtho(false, BOARD_SIZE, BOARD_SIZE);
 
-    /** Used after a pawn promotion to preserve board state + flipY flag. */
-    public GameScreen(ChessGame game, ChessBoard board) {
-        batch      = new SpriteBatch();
-        camera     = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 800);
-        chessBoard = board;
-        Gdx.input.setInputProcessor(new ChessInputProcessor(game, chessBoard, camera));
+        this.model          = model;
+        this.renderer       = renderer;
+        this.inputProcessor = new ChessInputProcessor(game, model, camera, renderer);
+        Gdx.input.setInputProcessor(inputProcessor);
     }
 
     @Override
     public void render(float delta) {
-        chessBoard.update(delta);
         Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         camera.update();
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.setColor(1f,1f,1f,0.7f);
-        chessBoard.render(batch);
+        renderer.render(batch);
         batch.end();
     }
 
@@ -60,8 +73,10 @@ public class GameScreen implements Screen {
     @Override public void hide()    { }
     @Override public void pause()   { }
     @Override public void resume()  { }
-    @Override public void dispose() {
+
+    @Override
+    public void dispose() {
         batch.dispose();
-        chessBoard.dispose();
+        renderer.dispose();
     }
 }
