5958ac5859c3039df3e427bba9c148085770eb80
Darhan_Semiatov <Darhansemyatov1@gmail.com>
2025-04-30 12:22:28 +0500

    Create IBoardModel.java, Abstraction For Board

---
+++ core/src/main/java/com/mygdx/chess/model/IBoardModel.java
@@ -0,0 +1,19 @@
+
+package com.mygdx.chess.model;
+
+import com.mygdx.chess.actors.ChessPiece;
+import com.mygdx.chess.logic.GameLogic;
+import com.mygdx.chess.logic.Move;
+
+import java.util.List;
+
+/**
+ * Abstraction for board state and game logic.
+ */
+public interface IBoardModel {
+    List<ChessPiece> getPieces();
+    GameLogic getGameLogic();
+    boolean isFlipped();
+    List<Move> getPossibleMoves();
+    void       setPossibleMoves(List<Move> moves);
+}
