200668bff3b97a4685813e130227153f5ff02c36
aloneen <seisenbek2005@mail.ru>
2025-04-30 11:33:41 +0500

    Modified Main Menu for Bot's sections

--- core/src/main/java/com/mygdx/chess/screens/MainMenuScreen.java
+++ core/src/main/java/com/mygdx/chess/screens/MainMenuScreen.java
@@ -1,4 +1,3 @@
-
 package com.mygdx.chess.screens;
 
 import com.badlogic.gdx.Gdx;
@@ -12,33 +11,30 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.chess.ChessGame;
 
 public class MainMenuScreen implements Screen {
     private final ChessGame game;
-    private final Stage stage;
-    private final Skin skin;
+    private final Stage     stage;
+    private final Skin      skin;
 
     public MainMenuScreen(ChessGame game) {
         this.game  = game;
         this.stage = new Stage(new ScreenViewport());
         this.skin  = new Skin(Gdx.files.internal("skins/uiskin.json"));
 
-        // 1) Background image
+        // Background
         Image bg = new Image(new com.badlogic.gdx.graphics.Texture(Gdx.files.internal("images/main_bg.jpg")));
         bg.setFillParent(true);
         stage.addActor(bg);
 
-        // 2) UI table on top
         Table table = new Table();
         table.setFillParent(true);
         stage.addActor(table);
 
         Label title = new Label("My Chess Game", skin);
         title.setFontScale(2f);
-        title.setAlignment(Align.center);
 
         TextButton whiteBtn = new TextButton("Play as White", skin);
         whiteBtn.addListener(new ClickListener() {
@@ -54,22 +50,26 @@ public class MainMenuScreen implements Screen {
             }
         });
 
+        TextButton vsBotBtn = new TextButton("Play vs Bot", skin);
+        vsBotBtn.addListener(new ClickListener() {
+            @Override public void clicked(InputEvent e, float x, float y) {
+                game.setScreen(new BotLevelScreen(game));
+            }
+        });
+
         table.add(title).padBottom(40f).row();
         table.add(whiteBtn).width(200).pad(10).row();
-        table.add(blackBtn).width(200).pad(10);
+        table.add(blackBtn).width(200).pad(10).row();
+        table.add(vsBotBtn).width(200).pad(10);
     }
 
-    @Override public void show() {
-        Gdx.input.setInputProcessor(stage);
-    }
-
-    @Override public void render(float delta) {
+    @Override public void show()                 { Gdx.input.setInputProcessor(stage); }
+    @Override public void render(float dt)       {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(delta);
+        stage.act(dt);
         stage.draw();
     }
-
     @Override public void resize(int w,int h)    { stage.getViewport().update(w,h,true); }
     @Override public void pause()                { }
     @Override public void resume()               { }
