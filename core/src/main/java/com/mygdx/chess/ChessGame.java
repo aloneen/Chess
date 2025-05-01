ab6b3043a15a15afc88f54551d5d5773d3bf39b3
Darhan_Semiatov <Darhansemyatov1@gmail.com>
2025-05-01 11:26:34 +0500

    Update ChessGame.java For Proxy pattern usage

--- core/src/main/java/com/mygdx/chess/ChessGame.java
+++ core/src/main/java/com/mygdx/chess/ChessGame.java
@@ -1,20 +1,36 @@
+
 package com.mygdx.chess;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
-
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.chess.proxy.TextureProxy;
 import com.mygdx.chess.screens.MainMenuScreen;
 
+/**
+ * The main entry point for the LibGDX chess game.
+ */
 public class ChessGame extends Game {
+    private SpriteBatch batch;
+
     @Override
     public void create() {
+        batch = new SpriteBatch();
+        // Launch the main menu screen
         setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render(); // delegate to current screen's render method
+    }
+
+    @Override
+    public void dispose() {
+        TextureProxy.disposeAll(); // PROXY: after running dispose all textures.
+    
+        if (batch != null) batch.dispose();
+        // Screens and their assets will be disposed by LibGDX when setScreen is changed or on exit
+        super.dispose();
     }
 }
